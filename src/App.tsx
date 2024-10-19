79dd44529dabdd485514a10dc31d0f54982310a1
Azhari12 <ariazhari0897@gmail.com>
2024-10-19 10:46:54 +0700

    feat: add dark mode scope

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
 	return (
-		<div className="min-h-screen">
-			<RouterProvider router={router} />
-		</div>
+		<ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+			<div className="min-h-screen">
+				<RouterProvider router={router} />
+			</div>
+		</ThemeProvider>
 	);
 }
 
